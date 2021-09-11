e9afbc3bb7092ceb30ad2c25e57920c3a2776e2b
PUELLESR <sagronald125@gmail.com>
2021-09-11 14:46:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const _express = require('express');
 const _server = _express();
 
 const _port = 4000;
-const cors = require('cors');
-app.use(cors({
-    origin: 'http://alb-ibm-web-472115302.us-west-2.elb.amazonaws.com/'
-}));
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+//const cors = require('cors');
+//app.use(cors({
+//    origin: 'http://alb-ibm-web-472115302.us-west-2.elb.amazonaws.com/'
+//}));
+
 //header('access-Control-Allow-Origin:*');
 _server.get('/retoibm/sumar/:sumando01/:sumando02', function(request, response) {
   try{

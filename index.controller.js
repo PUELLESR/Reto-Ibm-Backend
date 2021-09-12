4aa077b8fc1cc0e5525e2425a5a41a9a3ff39cb8
PUELLESR <sagronald125@gmail.com>
2021-09-12 02:08:47 -0500

    update CI/CD

--- index.controller.js
+++ index.controller.js
@@ -6,22 +6,3 @@ const poll = new Pool ({
     database: 'operacion',
     port: '5432'
 });
-
-/*const createUser = async (req, res) => {
-    const { _sumando01, _sumando02, _resultado } = req.body;
-
-    const response = await pool.query('INSERT INTO operacion (sumando01, sumando02, resultado) VALUES ($_sumando01, $_sumando02, $_resultado)', [_sumando01, _sumando02, _resultado])
-    res.sent('operacion ok')
-}
-
-const getSuma = async (req, res) => {
-    const response = await pool.query('SELECT * FROM sumando');
-    res.status(200).json(response.rows);
-}
-
-module.exports = {
-    createUser,
-    getSuma
-
-}
-*/
--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const _express = require('express');
 const _server = _express();
 const { Router } = require('express');
 const router = Router();
-
-//const { createUser, getSuma} = require('index.controller.js');
-
 const app = _express();
 
 const { Pool } = require('pg');
@@ -18,13 +15,6 @@ const poll = new Pool ({
 
 app.use(_express.json());
 
-//router
-//app.use(require('index.controller.js'));
-
-//middlewares
-//router.get('/users', getSuma);
-//router.post('/users', createUser);
-
 module.exports = router;
 
 const _port = 4000;
@@ -37,30 +27,14 @@ _server.use((req, res, next) => {
   next();
 });
 
-const sq = 'INSERT INTO sumando (sumando01, sumando02, resultado) VALUES ($1, $2, $3)';
-const val = []; 
-
-const insertSuma = async (sq,val) => {
-  try {
-    const res = await poll.query(sq,val);
-    console.log(res);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 _server.get('/retoibm/sumar/:sumando01/:sumando02', function(request, response) {
   try{
     var _sumando01 = new Number(request.params.sumando01);
     var _sumando02 = new Number(request.params.sumando02);
     var _resultado = _sumando01 + _sumando02;
     poll.query('INSERT INTO sumando (sumando01, sumando02, resultado) VALUES ($1, $2, $3)', [_sumando01,_sumando02,_resultado]);
-//    val = [_sumando01, _sumando02, _resultado];
-//    insertSuma(sq,val);
-//    poll.end();
 
     if (typeof _resultado !== "undefined" && _resultado!==null && !isNaN(_resultado)){    
-      //poll.query('INSERT INTO sumando (sumando01, sumando02, resultado) VALUES (3, 4, 7)');
       return response.status(200).json({resultado : _resultado});
     }else{
       return response.status(400).json({resultado : "Bad Request"});
@@ -70,25 +44,6 @@ _server.get('/retoibm/sumar/:sumando01/:sumando02', function(request, response) 
     return response.status(500).json({resultado : e});
   }
 });
-
-/*_server.post('/retoibm/sumar/:sumando01/:sumando02', function(request, response) {
-  try{
-    var _sumando01 = new Number(request.params.sumando01);
-    var _sumando02 = new Number(request.params.sumando02);
-    var _resultado = _sumando01 + _sumando02;
-        
-    if (typeof _resultado !== "undefined" && _resultado!==null && !isNaN(_resultado)){    
-      //pool.query('INSERT INTO sumando (sumando01, sumando02, resultado) VALUES ($_sumando01, $_sumando02, $_resultado)', [_sumando01, _sumando02, _resultado]);
-      poll.query('INSERT INTO sumando (sumando01, sumando02, resultado) VALUES (3, 4, 7)');
-    }else{
-      return response.status(400).json({resultado : "Bad Request"});
-    }
-  }
-  catch(e){
-    return response.status(500).json({resultado : e});
-  }
-});*/
-
 
 _server.listen(_port, () => {
    console.log(`Server listening at ${_port}`);
